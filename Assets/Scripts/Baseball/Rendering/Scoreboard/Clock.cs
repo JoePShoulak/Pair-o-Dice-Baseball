29d4be1688900f68fd3ae67a24e33338b77bd780
Joe P Shoulak <joe@squarecompass.io>
2023-08-31 10:42:46 -0500

    Clock works

--- Assets/Scripts/Baseball/Rendering/Scoreboard/Clock.cs
+++ Assets/Scripts/Baseball/Rendering/Scoreboard/Clock.cs
@@ -5,18 +5,20 @@ namespace FibDev.Baseball.Rendering.Scoreboard
 {
     public class Clock : MonoBehaviour
     {
-        [SerializeField] private Transform minuteHand;
         [SerializeField] private Transform hourHand;
+        [SerializeField] private Transform minuteHand;
+        [SerializeField] private Transform secondHand;
 
-        private void Start()
+        private void Update()
         {
-            var hours = DateTime.Now.Hour;
-            var minutes = DateTime.Now.Minute;
-            var seconds = DateTime.Now.Second;
+            var millis = DateTime.Now.Millisecond;
+            var seconds = DateTime.Now.Second + millis / 1000f;
+            var minutes = DateTime.Now.Minute + seconds / 60f;
+            var hours = DateTime.Now.Hour + minutes / 60f;
             
-            Debug.Log(hours);
-            Debug.Log(minutes);
-            Debug.Log(seconds);
+            hourHand.eulerAngles = new Vector3(0, 0, -hours * 30);
+            minuteHand.eulerAngles = new Vector3(0, 0, -minutes * 6);
+            secondHand.eulerAngles = new Vector3(0, 0, -seconds * 6);
         }
     }
 }
